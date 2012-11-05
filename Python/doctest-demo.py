86d8431e81071f80466c17be5955ea3349cbede3
unknown <rkiran@TA54577-7.fry.com>
2012-11-05 17:29:55 -0600

    doctest

---
+++ Python/doctest-demo.py
@@ -0,0 +1,15 @@
+"""
+>>> aList=["this", "is", "great"]
+>>> aList[0]
+'this'
+"""
+def fun():
+    """
+    >>> fun()
+	'abc'
+	"""
+	return 'abc'
+
+	if __name__ == '__main__':
+		import doctest
+		doctest.testmod()
