aceea0d4b585a7bea2c761f33f9bb570a2a4e46f
Ravi Kiran <rkiran3@yahoo.com>
2013-03-01 18:26:59 -0600

    Add examples

--- Python/builtins-map.py
+++ Python/builtins-map.py
@@ -1,6 +1,17 @@
 #!c:/apps/python32/python.exe
 
 ################## map #############################
+# return a modified value 
+def getModifiedPrice(price):
+    """
+    >>> list(map(getModifiedPrice, range(1,5)))
+    [1.1, 2.2, 3.3, 4.4]
+    """
+    modifier = 1.1
+	# convert to floating point number with 2 digits after decimal point
+    return round(price*modifier, 2)       
+
+################## map #############################
 fd = open("/tmp/price.txt")   # contains tab delimited rows
 # return a value that exists at a particular column in input string 
 def getPrice(line):
