30cd2b81637139d092c265b270220ea26a9ac94f
Ravi Kiran <rkiran3@yahoo.com>
2013-03-08 22:28:20 -0600

    Add examples

--- Python/builtins.py
+++ Python/builtins.py
@@ -14,3 +14,11 @@ sorted_names = sorted(names)
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+
+################## sorted #############################
+>>> import random
+>>> [ random.randint(1,x) for x in range(1,15)]
+[1, 2, 3, 3, 2, 5, 4, 4, 6, 9, 10, 2, 10, 12]
+>>> sorted([ random.randint(1,x) for x in range(1,15)])
+[1, 1, 2, 2, 2, 3, 3, 3, 6, 6, 6, 7, 9, 10]
+>>>
