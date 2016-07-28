6f1b46ed8539d398363ae246273a6baae0768739
ravikir <ravikir>
2016-07-28 14:46:58 -0500

    Clean up

--- Java/src/com/sandbox/java7/nio/FileReadDemo.java
+++ Java/src/com/sandbox/java7/nio/FileReadDemo.java
@@ -16,37 +16,14 @@ public class FileReadDemo {
 
         try {
             Map skuMapping = new HashMap<String, String>();
-            String fileName = "D:\\CrashPlanBackupFolder\\JADocs\\ItemNumberConversion\\JA_Prod_NewItemNumbers_07262016.txt";
+            String fileName = "D:\\demo.txt";
             Charset charset = Charset.forName("US-ASCII");
             int index = 0;
             Map <String, Integer> newSKUMap = new HashMap<String, Integer>();
 
             for (String line : Files.readAllLines(Paths.get(fileName), charset)) {
                 // ...
-                // Ignore the first record
-                if (index++==0) {
-                    continue;
-                }
-                String [] tokens = line.split("\\t");
-                if (tokens == null || tokens.length < 3) {
-                    System.out.println("Error in input, token is null or length < 3 ");
-                } else {
-                    String oldSKU = tokens[0];
-                    String newSKU = tokens[1];
-
-                    if (newSKUMap.containsKey(newSKU)) {
-                        System.out.println("there is a duplicate newSKU");
-                    } else {
-                        newSKUMap.put(newSKU, 0);
-                    }
-
-                    if (skuMapping.containsKey(oldSKU)) {
-                        System.out.println("there is a duplicate oldSKU");
-                    } else {
-                        skuMapping.put(oldSKU, newSKU);
-                    }
-                }
-                //System.out.println(line);
+                System.out.println(line);
             }
         } catch (IOException ioEx) {
 
