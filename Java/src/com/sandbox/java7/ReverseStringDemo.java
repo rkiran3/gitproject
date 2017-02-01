9df6e08165b6eba902fdb7c37b96956c63ef7493
ravikir <ravikir>
2017-01-31 22:08:08 -0600

    Formatting changes only

--- Java/src/com/sandbox/java7/ReverseStringDemo.java
+++ Java/src/com/sandbox/java7/ReverseStringDemo.java
@@ -10,24 +10,23 @@ This program will reverse a string
 
 */
 public class ReverseStringDemo {
-	
-	public static void main(String [] args) {
 
-		String message = "Hello, World";
-		
-		char [] messageChars = message.toCharArray();
-		// 
-		int begin=0;
-		int end = messageChars.length-1;
-		while (begin < end) {
-			System.out.println("char: " + messageChars[begin]);
-			char temp = messageChars[begin];
-			messageChars[begin] = messageChars[end];
-			begin++;
-			end--;
-		}
-		
-		
-		System.out.println(messageChars.toString());
-	}
+    public static void main(String [] args) {
+
+    String message = "Hello, World";
+    System.out.println("Processing String: " + message);
+    char [] messageChars = message.toCharArray();
+    //
+    int begin=0;
+    int end = messageChars.length-1;
+    while (begin < end) {
+        System.out.println("Processing: " + messageChars[begin]);
+        char temp = messageChars[begin];
+        messageChars[begin] = messageChars[end];
+        messageChars[end] = temp;
+        begin++;
+        end--;
+    }
+    System.out.println(new String(messageChars));
+    }
 }
