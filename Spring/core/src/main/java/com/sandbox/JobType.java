0608cfc6f4278e1ec2e87fb37c410bfc77c3ccc0
ravikir <ravikir>
2017-02-28 16:25:34 -0600

    Adding Spring Files

---
+++ Spring/core/src/main/java/com/sandbox/JobType.java
@@ -0,0 +1,17 @@
+package com.sandbox;
+
+/**
+ * Created by ravikir on 2/27/2017.
+ */
+public class JobType {
+    String name;
+
+    public String getName() {
+        return name ;
+    }
+
+    public void setName(String name) {
+        this. name = name;
+    }
+
+}
