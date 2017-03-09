d7e14ddee82d994e6ce9849ba8f107b1003fd0bb
ravikir <ravikir>
2017-03-09 15:03:18 -0600

    Additional files added

---
+++ JavaExamples/src/main/java/com/spoj/PALIN.java
@@ -0,0 +1,42 @@
+package com.spoj;
+
+/**
+ * Created by ravikir on 3/9/2017.
+ */
+public class PALIN {
+
+    public String reverse(String input) {
+        String reverseStr = "";
+
+        return new StringBuilder(input).reverse().toString();
+    }
+
+    /**
+     * Given an input integer, find the next number which is a palindrome,
+     * Example: input: 808  iterate to next number which is a palindrome 818
+     * @param k
+     * @return next number which is Palindrome.
+     */
+    public int smallestPalindrome(int k) {
+        String result = "0";
+        boolean found = false;  // initial flag
+
+        if (k > 0) {
+            while (!found) {
+                k++;
+                String numStr = String.valueOf(k);
+                // reverse the string and compare with original string
+                if (numStr.equals(reverse(numStr))) {
+                    found=true;
+                    break; // assign the found value
+                }
+            }
+        }
+        return k;
+    }
+
+    public static void main(String [] args) {
+        PALIN palin = new PALIN();
+        System.out.println(palin.smallestPalindrome(2133));
+    }
+}
