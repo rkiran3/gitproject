d6e00501183fd5fd16e728d7081842d49c320b17
Kiran <rkiran3@yahoo.com>
2019-04-13 18:43:57 -0500

    Add Functional exercises for codingbat.

--- JavaExamples/src/main/java/com/codingbat/ap1/CopyEndy.java
+++ JavaExamples/src/main/java/com/codingbat/ap1/CopyEndy.java
@@ -1,6 +1,5 @@
 package com.codingbat.ap1;
 
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,9 +7,9 @@ import java.util.stream.IntStream;
 /**
  * We'll say that a positive int n is "endy" if it is in the range 0..10 or 90..100 (inclusive). Given an array of positive ints, return a new array of length "count" containing the first endy numbers from the original array. Decompose out a separate isEndy(int n) method to test if a number is endy. The original array will contain at least "count" endy numbers.
 
- copyEndy([9, 11, 90, 22, 6], 2) → [9, 90]
- copyEndy([9, 11, 90, 22, 6], 3) → [9, 90, 6]
- copyEndy([12, 1, 1, 13, 0, 20], 2) → [1, 1]
+ copyEndy([9, 11, 90, 22, 6], 2) => [9, 90]
+ copyEndy([9, 11, 90, 22, 6], 3) => [9, 90, 6]
+ copyEndy([12, 1, 1, 13, 0, 20], 2) => [1, 1]
  */
 public class CopyEndy {
     public static Predicate<Integer> isEndyPredicate = n -> (n >=0 && n <10) || (n>=90 && n<=100);
@@ -54,6 +53,5 @@ public class CopyEndy {
                 IntStream.of(copyEndy(nums, 2))
                         .mapToObj(Integer::toString)
                         .collect(Collectors.joining(", ", "[", "]")));  // prints 6, 2, 4
-
     }
 }
