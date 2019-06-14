34ec55a544ec002935c8ed1d2978c7e58816c741
Kiran <rkiran3@yahoo.com>
2019-06-13 22:28:23 -0500

    Add Comparator Demo Class

--- JavaExamples/src/main/java/com/sandbox/java8/ComparatorExercise01.java
+++ JavaExamples/src/main/java/com/sandbox/java8/ComparatorExercise01.java
@@ -1,5 +1,6 @@
 package com.sandbox.java8;
 
+import static org.junit.Assert.assertArrayEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -43,50 +44,75 @@ class Person {
 public class ComparatorExercise01 {
 
     public static void main(String [] args) {
-        final List<Person> peopleList = Arrays.asList(
-                new Person("John", 20),
-                new Person("Sara", 21),
-                new Person("Jane", 21),
-                new Person("Greg", 35)
+        Person john = new Person("John", 20);
+        Person sara = new Person("Sara", 21);
+        Person jane = new Person("Jane", 21);
+        Person greg = new Person("Greg", 35);
+        final List<Person> personList = Arrays.asList(
+                john, sara, jane, greg
         );
 
-        Comparator<Person> compareAscending =
+        // Compare by Name
+        // specify pre-sorted list to compare after sorting
+        Person [] sortedPersonByName = new Person [] {
+                greg, jane, john, sara 
+                }; 
+        
+        // Define comparator for Names, sort and validate
+        Comparator <Person> personNameComparator = Comparator.comparing(Person::getName);        
+        Person [] personArray = personList.toArray(new Person[personList.size()]);
+        Arrays.sort(personArray, personNameComparator);
+        assertArrayEquals(personArray, sortedPersonByName);
+        
+        
+        // Define Comparator by Age, sort and validate
+        Comparator<Person> compareAgeAscending =
                 (person1, person2) -> person1.ageDifference(person2);
-        Comparator<Person> compareDescending = compareAscending.reversed();
-
-        List <Person> ascendingAge = peopleList
+        Comparator<Person> compareAgeDescending = compareAgeAscending.reversed();
+        Arrays.sort(personArray, compareAgeAscending);
+        Person [] sortedPersonByAgeAsc = new Person [] {
+                john, jane,  sara, greg // john=20, jane=21, sara=21, greg=35
+                }; 
+        assertArrayEquals(personArray, sortedPersonByAgeAsc);
+        
+        List <Person> ascendingAge = personList
                 .stream()
                 .sorted((person1, person2) -> person1.ageDifference(person2))
                 .collect(Collectors.toList());
+        //ascendingAge.forEach(System.out::println);
 
-        ascendingAge.forEach(System.out::println);
-
-        List <Person> ascendingAge2 = peopleList
+        List <Person> ascendingAge2 = personList
                 .stream()
-                .sorted(compareAscending)
+                .sorted(compareAgeAscending)
                 .collect(Collectors.toList());
-        ascendingAge2.forEach(System.out::println);
+        //ascendingAge2.forEach(System.out::println);
 
-        List <Person> descendingAge = peopleList
+        Arrays.sort(personArray, compareAgeDescending);
+        Person [] sortedPersonByAgeDesc = new Person [] {
+                greg, jane, sara, john // john=20, jane=21, sara=21, greg=35
+                };         
+        assertArrayEquals(personArray, sortedPersonByAgeDesc);
+
+
+        List <Person> descendingAge = personList
                 .stream()
-                .sorted(compareDescending)
+                .sorted(compareAgeDescending)
                 .collect(Collectors.toList());
-        descendingAge.forEach(System.out::println);
+        //descendingAge.forEach(System.out::println);
 
-        System.out.println("Print persons older than 20");
+        //System.out.println("Print persons older than 20");
         List <Person> olderThan20 = new ArrayList<>();
-        peopleList
+        personList
                 .stream()
                 .filter(person -> person.getAge() > 20)
                 .forEach(person -> olderThan20.add(person));
-        olderThan20.forEach(System.out::println);
+        //olderThan20.forEach(System.out::println);
 
-        System.out.println("Older than 20 using collect");
-        peopleList
+        //System.out.println("Older than 20 using collect");
+        ArrayList<Person> personListFromStream = personList
                 .stream()
                 .filter(person -> person.getAge() > 20)
                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-        olderThan20.forEach(System.out::println);
-
+        //olderThan20.forEach(System.out::println);
     }
 }
--- cookbook/python/toc.md
+++ cookbook/python/toc.md
@@ -2,6 +2,4 @@
 
 ### [Working with Maps](./workingwithmaps.md)
 
-### [Matching with Streams](./streamsmatching.md)
-
-### [Demo](./demo.md)
+### [Working with JSON](./workingwithjson.md)
