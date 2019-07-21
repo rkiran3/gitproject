1130854b0f5d2da0f52dec90c5f5945e23a01459
Ravi Kiran <rkiran3@yahoo.com>
2019-07-20 20:50:36 -0500

    Add Java Exercise

--- JavaExamples/src/main/java/com/sandbox/java8/CollectorExercise04.java
+++ JavaExamples/src/main/java/com/sandbox/java8/CollectorExercise04.java
@@ -5,32 +5,32 @@ import java.util.stream.*;
 import java.util.*;
 
 /**
-	* collect only entries in a multiline
-  * that do not start with an identifier.
+    * collect only entries in a multiline
+    * that do not start with an identifier.
  */
 
 public class CollectorExercise04 {
 
-	// create a multiline that has an identifier
-	// input
-	public static String demoStr = "--Firstline\n" +
+    // create a multiline that has an identifier
+    // input
+    public static String demoStr = "--Firstline\n" +
 "1100 \n" +
 "2200 \n" +
 "--SecondLine\n" +
 "3333\n" +
 "4444\n";
-	
+
     public static void main(String[] args) {
     
-			List <String> entries = new ArrayList<String> ();
+        List <String> entries = new ArrayList<String> ();
 
-			String [] tokens = 	demoStr.split("\n");
-			//System.out.println(tokens);
-			List <String> accounts = Arrays.asList(tokens).stream()
-				.filter(w -> !w.startsWith("--"))
-				.collect(Collectors.toList());
+        String [] tokens = demoStr.split("\n");
+        //System.out.println(tokens);
+        List <String> accounts = Arrays.asList(tokens).stream()
+            .filter(w -> !w.startsWith("--"))
+            .collect(Collectors.toList());
 
-			// [1100, 2200, 3333, 4444]
-			System.out.println(accounts);
+        // [1100, 2200, 3333, 4444]
+        System.out.println(accounts);
     }
 }
