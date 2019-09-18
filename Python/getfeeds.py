8e57e0c5415315bddb167510e182e25cb0876971
Kiran <rkiran3@yahoo.com>
2019-09-18 09:22:35 -0500

    Adding updates

--- Python/getfeeds.py
+++ Python/getfeeds.py
@@ -3,21 +3,30 @@
 import feedparser
 import sys
 
-url = 'http://feeds2.feedburner.com/ManagementRss'
+urlDict = {
+    
+    'key':'url of feed'
+}
 
-json_feed  = feedparser.parse(url)
-# works
-#print (json_feed['entries'][0]['summary_detail'])
-#print (json_feed['entries'][0]['links'])
-#print len(json_feed['entries'])
+for key, value in urlDict.items():
+    print ('Processing ', key, '->', value)
+        
+    url = value
 
-#sys.exit(-1)
-for entry in json_feed['entries']:
-    print (entry['links'])
-    #print (entry['summary_detail'])
-    print (entry['title'])
-    print (entry['content'])
-    print ("----------\n")
+    json_feed  = feedparser.parse(url)
+    # works
+    #print (json_feed['entries'][0]['summary_detail'])
+    #print (json_feed['entries'][0]['links'])
+    #print len(json_feed['entries'])
+
+    #sys.exit(-1)
+    for entry in json_feed['entries']:
+        #print (entry['links'])
+        #print (entry['summary_detail'])
+        print ("Title: " + entry['title'])
+        print (entry['description'])
+        #print (entry['subtitle'])
+        print ("----------\n")
 
 #print (json_obj.feed.title)
 
