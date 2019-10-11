40dc2fdc5da5af82b0d9030d926af835636c123c
Kiran <rkiran3@yahoo.com>
2019-10-11 08:50:42 -0500

    Add Exercises

--- JavaExamples/src/main/java/com/sandbox/java8/FilterExercise02.java
+++ JavaExamples/src/main/java/com/sandbox/java8/FilterExercise02.java
@@ -5,6 +5,7 @@ package com.sandbox.java8;
 //import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -41,15 +42,102 @@ public class FilterExercise02 {
         //assertArrayEquals(expected, monthsEndsY.toArray());
         
         // another example to filter out null values
+        // Input: { "January": null,   "February": Integer(28), "March": null }
+        // Output:  { "February": Integer(28) }
         Map <String, Object> dict = new LinkedHashMap<>();
         dict.put("January", null);
         dict.put("February", new Integer(28));
-        dict.put("March", null);
-        
+        dict.put("March", null);        
         Map <String, Object> newMap = dict.entrySet().stream()
             .filter(m -> m.getValue() != null)
             .collect(
                     Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        // Filter map entries and get the value associated with Key 
+        // Input: { "December": "Cold", "January": "Colder", "February" : "Coldest" }
+        // key = February
+        // expected: "Coldest"
+        Map <String, String> demoMap = new HashMap<>();
+        demoMap.put("December", "Cold");
+        demoMap.put("January", "Colder");
+        demoMap.put("February", "Coldest");
+        final String monthKey = "February";
+        String condition = demoMap.entrySet().stream()
+            .filter(m -> m.getKey().equals(monthKey))
+            .map(m -> m.getValue())
+            .findFirst()            
+            .orElse(null);
+        System.out.println(condition);
+        if (!condition.equals("Coldest")) {
+            System.out.println("Test Fails, expected Coldest for month February");
+        }        
+        
+        // Input: { "December": "Cold", "January": "Colder", "February" : "Coldest" }
+        // key = January
+        // expected: "Colder"
+        final String monthKey2 = "January";
+        condition = demoMap.entrySet().stream()
+            .filter(m -> m.getKey().equals(monthKey2))
+            .map(m -> m.getValue())
+            .findFirst()            
+            .orElse(null);
+        System.out.println(condition);
+        if (!condition.equals("Colder")) {
+            //System.out.println("Test Fails, expected Coldest for month February");
+        }
+        
+
+        // Input: { "December": "Cold", "January": "Colder", "February" : "Coldest" }
+        // check for non exist key = March
+        // expected: null
+        final String nonexistmonth = "March";
+        condition = demoMap.entrySet().stream()
+            .filter(m -> m.getKey().equals(nonexistmonth))
+            .map(m -> m.getValue())
+            .findFirst()            
+            .orElse(null);
+        System.out.println(condition);
+        if (condition != null) {
+            System.out.println("Test Fails, expected null");
+        }
+        
+        
+        // Filter map entries and get the value associated with Key 
+        Map <String, String> demoMap2 = new HashMap<>();
+        demoMap2.put("December", "Cold   ");
+        demoMap2.put("January", "Colder ");
+        demoMap2.put("February", "Coldest               ");
+        
+        demoMap2.entrySet()
+            .forEach(entry -> System.out.printf("[%s]\n", entry.getValue()));
+        
+        demoMap2.entrySet().forEach( entry -> 
+            entry.setValue(entry.getValue().trim()));
+        
+        demoMap2.entrySet()
+            .forEach(entry -> System.out.printf("[%s]\n", entry.getValue()));
+        
+        demoMap2.entrySet().stream()
+            .filter(entry -> entry.getValue() instanceof String)
+            //.forEach(entry -> System.out.printf("[%s]\n", entry.getValue()));
+            .forEach(entry -> entry.setValue(entry.getValue().trim()));
+
+        // Filter map entries and get the value associated with Key 
+        Map <String, Object> demoMap3 = new HashMap<>();
+        demoMap3.put("December", null);
+        demoMap3.put("January", "Colder         ");
+        demoMap3.put("February", new Integer(23));
+
+        demoMap3.entrySet().stream()
+            .filter(entry -> entry.getValue() instanceof String)
+            //  .forEach(entry -> System.out.printf("[%s]\n", entry.getValue()));
+            .forEach(entry -> entry.setValue(((String)entry.getValue()).trim()));
+
+        demoMap3.entrySet().stream()
+            .filter(entry -> entry.getValue() instanceof String)
+            //.forEach(entry -> System.out.printf("[%s]\n", entry.getValue()));
+            .forEach(entry -> System.out.println(entry.getValue()));
+
         
         //assertTrue(newMap.size() ==  1);
         //assertTrue(newMap.containsKey("February"));
