8b424e2dfc2c90bcdb0d40e8333f58ade1b7982f
Ravi Kiran <rkiran3@yahoo.com>
2020-05-06 07:54:11 -0500

    Additional examples added.

--- JavaExamples/src/main/java/com/codingbat/functional1/Lower.java
+++ JavaExamples/src/main/java/com/codingbat/functional1/Lower.java
@@ -5,7 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Lower case the very first character of each word
+ * Given a list of strings, return a list where each string is 
+ * converted to lower case (Note: String toLowerCase() method).
  */
 public class Lower {
     public static List <String> lower(List<String> strings) {
