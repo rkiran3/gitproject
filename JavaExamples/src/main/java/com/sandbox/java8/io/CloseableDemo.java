c2692f2df62aa5acae64da94267cf5749f3bde10
Ravi Kiran <rkiran3@yahoo.com>
2021-07-20 20:30:34 -0500

    Adding Closeable program.

---
+++ JavaExamples/src/main/java/com/sandbox/java8/io/CloseableDemo.java
@@ -0,0 +1,29 @@
+package com.sandbox.java8.io;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+class ItemCloseable implements Closeable {
+
+  @Override
+  public void close() throws IOException {
+    System.out.println("ItemCloseable close()");
+  }
+}
+
+public class CloseableDemo {
+
+  public static void main(String[] args) {
+
+    System.out.println("Start");
+    ItemCloseable ic = new ItemCloseable();
+    try (ic){
+      ic.close();
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
+  }
+
+}
