3b03770377bab93c45870214f138ed106b03959d
Ravi Kiran <rkiran3@yahoo.com>
2021-07-31 12:54:14 -0500

    Cleanup

--- JavaExamples/src/main/java/com/algodaily/DeletePriorChar.java
+++ JavaExamples/src/main/java/com/algodaily/DeletePriorChar.java
@@ -5,54 +5,51 @@ import java.util.List;
 
 /**
  * Delete prior character in a String if "$" is found
- * f$st  =>  st  
+ * f$st  =>  st
  * rout$    => rout
  */
 public class DeletePriorChar {
 
-	public static void churn() {
-		
-		java.util.stream.IntStream.range(0, 57)
-			.mapToObj(String::valueOf)
-			.forEach(i -> new String(i));
-		
-		return;
-	}
-	
+  public static void churn() {
+
+    java.util.stream.IntStream.range(0, 57)
+      .mapToObj(String::valueOf)
+      .forEach(i -> new String(i));
+    
+    return;
+  }
+
     public static String []deleteChar(String [] input) {
-        List <String> output = new ArrayList<String>(); 
+        List<String> output = new ArrayList<String>();
         int dIndex = 0;
         
         for (int sIndex=0; sIndex < input.length; sIndex++) {
-            if (input[sIndex].equals("$") || 
-                    ((sIndex+1 < input.length) && input[sIndex+1].equals("$"))) {
-                continue;
-            } 
-            
-            dIndex++;
-            output.add(input[sIndex]);
+          if (input[sIndex].equals("$") ||
+              ((sIndex+1 < input.length) && input[sIndex+1].equals("$"))) {
+            continue;
+          }
+          
+          dIndex++;
+          output.add(input[sIndex]);
         }
         
         return output.toArray(new String[dIndex]);
     }
-    public static void main(String[] args) {
-        //String [] input = { "f", "$", "s", "t" };        
-        //String [] output = deleteChar(input);
-        
-    	for (int i=0; i< Integer.MAX_VALUE; i++) {
-    		if (i < Integer.MAX_VALUE/2) {
-    			String demo = new String(String.valueOf(i));
-    		} else {
-    			System.out.println("Reached half way");
-    		}
-    		
-    	}
-    	
-        churn();
-        
-        System.out.println("End");
-        //for (int i=0; i< output.length; i++) {
-            //System.out.println(output[i]);
-        //}
+  public static void main(String[] args) {
+    //String [] input = { "f", "$", "s", "t" };
+    //String [] output = deleteChar(input);
+    
+    for (int i=0; i< Integer.MAX_VALUE; i++) {
+      if (i < Integer.MAX_VALUE/2) {
+        String demo = new String(String.valueOf(i));
+      } else {
+        System.out.println("Reached half way");
+      }
+      
     }
+    churn();
+    
+    System.out.println("End");
+
+  }
 }
--- Python/builtins-map.py
+++ Python/builtins-map.py
@@ -1,6 +1,7 @@
-#!c:/apps/python32/python.exe
+#!/usr/bin/python
 
-################## map #############################
+# map #
+
 # return a modified value
 def getModifiedPrice(price):
     """
@@ -11,18 +12,25 @@ def getModifiedPrice(price):
     # convert to floating point number with 2 digits after decimal point
     return round(price*modifier, 2)
 
-################## map #############################
+# map #
+
+
 fd = open("/tmp/price.txt")   # contains tab delimited rows
 # return a value that exists at a particular column in input string
+
+
 def getPrice(line):
-    price = line.split("\t")[3] # price exists at index 3
+    price = line.split("\t")[3]  # price exists at index 3
     price = price.strip()   # remove spaces, newlines
     return int(price)       # convert to integer
 
 # get a list of int values
+
+
 # invoke getPrice() on each element in list
 priceList = list(map(getPrice, fd.readlines()))
-print (priceList) # prints a list of values found in the input file
+
+print(priceList)  # prints a list of values found in the input file
 
 if __name__ == "__main__":
     import doctest
