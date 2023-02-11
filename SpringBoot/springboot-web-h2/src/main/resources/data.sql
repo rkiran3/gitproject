06e2db3d97dd71629eb20aed27fd502531a97797
Ravi Kiran <rkiran3@yahoo.com>
2023-02-11 12:48:09 -0600

    Add data.sql to springboot-web-h2 project

---
+++ SpringBoot/springboot-web-h2/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  category VARCHAR(250) NOT NULL,
+  title VARCHAR(250) NOT NULL,
+  content VARCHAR(250) DEFAULT NULL
+);
+INSERT INTO posts (category, title, content) VALUES
+  ('Java', 'How to use anyMatch in Streams', 'anyMatch returns a boolean if it matches criteria'),
+  ('Emacs', 'How to bring up help', 'Hit Ctrl-h i which will bring up the help section');
+
