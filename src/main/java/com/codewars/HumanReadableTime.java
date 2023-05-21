37c31677f725b9c9c3c3073246e37efb0af7a5b7
Ravi Kiran <rkiran3@yahoo.com>
2023-05-21 16:40:16 -0500

    Format HumanReadableTime.java

--- src/main/java/com/codewars/HumanReadableTime.java
+++ src/main/java/com/codewars/HumanReadableTime.java
@@ -1,52 +1,53 @@
 package com.codewars;
 
+/** Construct a human readable time using seconds. */
 public class HumanReadableTime {
 
-	public static String makeReadable2(int input) {
-	    
-		long secs = (input % 60);
-		long mins = (input / 60);
-		long hrs = input / (60*60);
-		long mMins = mins % 60;
-		
-		String message = String.format("Secs: %03d  %02d:%02d:%02d", input, hrs, mMins, secs);
-		
-		
-		System.out.println(message);
-		
-	    return Integer.toString(input);
-	}
+  /** Given seconds, return a string that depicts the seconds. */
+  public static String makeReadable2(int input) {
 
-	public static String makeReadable(int input) {
-	    
-		int SECS = 60;
-		long MINS = 59*59;
-		long HRS = 59*59*59;
+    long secs = (input % 60);
+    long mins = (input / 60);
+    long hrs = input / (60 * 60);
+    long mmins = mins % 60;
 
-		long secs = (input % SECS);
-		long mins = (input / MINS);
-		//long minsMod = (input % MINS);
-		long hrs = 0;
-		
-		if (mins <= 60) {
-			String message = String.format("Secs: %2d  %02d:%02d:%02d", input, hrs, mins, secs);
-			System.out.println(message);
-		} else {
-			//secs = mins % SECS;
-			mins = mins % SECS;
-			hrs = mins % 60;
-			String message = String.format("Secs: %2d  %02d:%02d:%02d", input, hrs, mins, secs);
-			System.out.println(message);			
-		}
-		
-	    return Integer.toString(input);
-	}
+    String message = String.format("Secs: %03d  %02d:%02d:%02d", input, hrs, mmins, secs);
 
-	public static void main(String[] args) {
-		
-		for (int i=0; i <= 3601; i++) {
-			makeReadable2(i);
-		}
-	}
+    System.out.println(message);
+
+    return Integer.toString(input);
+  }
+
+  /** Given seconds, return a string that depicts the seconds. */
+  public static String makeReadable(int input) {
+
+    int secs = 60;
+    long mins = 59 * 59;
+    
+    long ssecs = (input % secs);
+    long mmins = (input / mins);
+    // long minsMod = (input % MINS);
+    long hhrs = 0;
+
+    if (mmins <= 60) {
+      String message = String.format("Secs: %2d  %02d:%02d:%02d", input, hhrs, mmins, ssecs);
+      System.out.println(message);
+    } else {
+      // secs = mins % SECS;
+      mmins = mins % secs;
+      hhrs = mins % 60;
+      String message = String.format("Secs: %2d  %02d:%02d:%02d", input, hhrs, mins, ssecs);
+      System.out.println(message);
+    }
+
+    return Integer.toString(input);
+  }
+
+  /** Main Program. */
+  public static void main(String[] args) {
+
+    for (int i = 0; i <= 60; i++) {
+      makeReadable2(i);
+    }
+  }
 }
-
