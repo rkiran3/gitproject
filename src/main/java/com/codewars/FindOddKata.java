c40bbdac7edd5013b7986925cff1d25d69dc840e
Ravi Kiran <rkiran3@yahoo.com>
2023-05-21 11:04:06 -0500

    Fix formatting with FindOddKata.java

--- src/main/java/com/codewars/FindOddKata.java
+++ src/main/java/com/codewars/FindOddKata.java
@@ -1,29 +1,29 @@
 package com.codewars;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import java.util.Arrays;
-
+/** Program to find odd number in array of elements. */
 public class FindOddKata {
 
-	public static void main(String[] args) {
-		System.out.println("Hello");
-		
-		// Create a list of elements, there will be one odd number and all others
-		// are in even number
-		List<Integer> myList = Arrays.asList(1, 1,  2, 3, 3);
-		
-		Map<Integer, Long> m = myList.stream()
-			.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		
-		for (Map.Entry<Integer, Long> me : m.entrySet()) {
-			System.out.println(me.getKey());
-			if (me.getValue() % 2 == 1) {
-				System.out.println("Found " + me.getKey());
-			}
-		}
-	}
+  /** Main Program. */
+  public static void main(String[] args) {
+
+    // Create a list of elements, there will be one odd number and all others
+    // are in even number
+    List<Integer> myList = Arrays.asList(1, 1, 2, 3, 3);
+
+    Map<Integer, Long> m = myList.stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+    for (Map.Entry<Integer, Long> me : m.entrySet()) {
+      System.out.println(me.getKey());
+      if (me.getValue() % 2 == 1) {
+        System.out.println("Found " + me.getKey());
+      }
+    }
+  }
 }
