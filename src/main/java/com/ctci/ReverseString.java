08edabc94b0207ea3844ca26de3130a92628dd99
Ravi Kiran <rkiran3@yahoo.com>
2023-05-26 11:48:04 -0500

    Apply formatting changes

--- src/main/java/com/ctci/ReverseString.java
+++ src/main/java/com/ctci/ReverseString.java
@@ -4,21 +4,21 @@ package com.ctci;
  *
  */
 public class ReverseString {
-    public static String reverse(String input) {
-        char [] outputArray = new char[input.length()];
-        char [] inputArray = input.toCharArray();
+  public static String reverse(String input) {
+    char[] outputArray = new char[input.length()];
+    char[] inputArray = input.toCharArray();
 
-        for (int i=0; i < input.length(); i++) {
-            int end = input.length()- i -1;
-            char tmpChar = inputArray[end];
-            outputArray[input.length() -i - 1] = inputArray[i];
-            inputArray[i] = tmpChar;
-        }
-
-        return new String(outputArray);
+    for (int i = 0; i < input.length(); i++) {
+      int end = input.length() - i - 1;
+      char tmpChar = inputArray[end];
+      outputArray[input.length() - i - 1] = inputArray[i];
+      inputArray[i] = tmpChar;
     }
 
-    public static void main(String [] args) {
-        System.out.println(reverse("PLAY"));
-    }
+    return new String(outputArray);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(reverse("PLAY"));
+  }
 }
