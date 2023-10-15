d2fe97f5ba9c1dc0dfaa2c7ac5942ec4ab2ecb97
Ravi Kiran <rkiran3@yahoo.com>
2023-10-15 17:44:07 -0500

    Refactor to support files and folders containing org files

--- src/main/java/com/sandbox/regex/ParseOrgFilesIntoTopics.java
+++ src/main/java/com/sandbox/regex/ParseOrgFilesIntoTopics.java
@@ -25,45 +25,72 @@ public class ParseOrgFilesIntoTopics {
         List<String> contentsList;
     }
 
+    public List<Snippet> processFile(Path path) {
+        List<Snippet> snippets = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(path);
+
+            // Org file topic 
+            String regex = "\\* .*"; // Header line
+            String regexDrill = ":.*"; // Drill
+            Snippet current = null;
+            boolean isHeader = false;
+            List<Snippet> snippetList = new ArrayList<>();
+            for (String line: lines) {
+                isHeader = line.matches(regex);
+
+                if (isHeader) {
+                    Snippet newSnippet = new Snippet();
+                    newSnippet.setHeader(line);
+                    newSnippet.setContentsList(new ArrayList<>());
+                    snippetList.add(newSnippet);
+                    current = newSnippet;
+                } else if (line.matches(regexDrill)) {
+                    continue;
+                } else if (current != null) {
+                    current.contentsList.add(line);
+                }
+            }
+
+            Snippet snippet = snippetList.get(new Random().nextInt(snippetList.size()));
+            System.out.println("====> [" + path + "]");
+            System.out.println(snippet.getHeader());
+            String contents = snippet.getContentsList().stream()
+                .collect(Collectors.joining("\n"));
+            System.out.println(contents);
+            } catch (Exception ex) {
+        }
+
+        return snippets;
+    }
+
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Please enter org file name to parse");
             System.exit(-1);
         }
         try {
+            ParseOrgFilesIntoTopics parseOrgFile = new ParseOrgFilesIntoTopics();
+
             Path path = Paths.get(args[0]);
+            List<Snippet> snippets = new ArrayList<>();
             if (path != null && path.getFileName() != null) {
-                List<String> lines = Files.readAllLines(path);
-                
-                // Org file topic 
-                String regex = "\\* .*";
-                Snippet current = null;
-                boolean isHeader = false;
-                List<Snippet> snippetList = new ArrayList<>();
-                for (String line: lines) {
-                    isHeader = line.matches(regex);
-
-                    if (isHeader) {
-                        Snippet newSnippet = new Snippet();
-                        newSnippet.setHeader(line);
-                        newSnippet.setContentsList(new ArrayList<>());
-                        snippetList.add(newSnippet);
-                        current = newSnippet;
-                    } else if (current != null) {
-                        current.contentsList.add(line);
-                    } else {
-                        //System.out.println("Ignoring line: " + line);
+                boolean isDir = Files.isDirectory(path);
+                if (isDir) {
+                    List<Path> files = Files.list(path).collect(Collectors.toList());
+                    for (Path entry : files) {
+                        if (entry.getFileName().toString().endsWith(".org")) {
+                            snippets.addAll(parseOrgFile.processFile(entry));
+                        }
                     }
+                } else {
+                    snippets.addAll(parseOrgFile.processFile(path));
                 }
-
-                Snippet snippet = snippetList.get(new Random().nextInt(snippetList.size()));
-                System.out.println(snippet.getHeader());
-                String contents = snippet.getContentsList().stream()
-                    .collect(Collectors.joining("\n"));
-                System.out.println(contents);
-            } else {
-                log.info("No file found");
             }
+
+            if (snippets.size() > 0) {
+                System.out.println("completed");
+            } 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
