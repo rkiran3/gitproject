395423dd4877f1cce8336df94c403dd9dde8359b
Ravi Kiran <rkiran3@yahoo.com>
2026-01-25 06:52:22 -0600

    Remove springboot-web-h2, replace by springbootwebh2

--- SpringBoot/springboot-web-h2/src/main/java/com/demo/sandbox/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.sandbox.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.sandbox.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- SpringBoot/springboot-web-h2/src/main/java/com/demo/sandbox/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.sandbox.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.sandbox.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
